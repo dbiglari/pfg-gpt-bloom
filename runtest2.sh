268f38f8c280f837452432a11e9dd11ab12e6965
Dorek Biglari <dbiglari@gmail.com>
2023-03-21 17:55:52 -0500

    update

--- runtest2.sh
+++ runtest2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
+#(sleep 1 && sudo chrt -p 99 $(pidof pfg-gpt-bloom) && sudo renice -n -20 -p $(pidof pfg-gpt-bloom))&
 
-
-./pfg-gpt-bloom -f $1 -t 1.0 -z 0.0 -m $3 -T $4 -F /etc/pfg_gpt_config -M $2 -C $5
+time ./pfg-gpt-bloom -f $1 -t 1.0 -z 0.0 -m $3 -T $4 -F /etc/pfg_gpt_config -M $2 -C $5
